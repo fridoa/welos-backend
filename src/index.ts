bdd37253334abb66092c6643176c2fec5ec725f8
fridoa <fridoafriyanto3@gmail.com>
2025-09-01 13:00:20 +0700

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import router from "./routes/api";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 4000;
+const { PORT } = process.env;
 
 app.get("/", (req, res) => {
   res.status(200).json({
