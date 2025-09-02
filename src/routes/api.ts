2327a6a6ab81a46b5c6c60a6019d32f05be3b590
fridoa <fridoafriyanto3@gmail.com>
2025-09-02 23:26:22 +0700

    refactor(auth): update login endpoint path

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -4,7 +4,7 @@ import authMiddleware from "../middlewares/auth.middleware";
 
 const router = express.Router();
 
-router.post("/register", authMiddleware.validateRegister, authController.register);
-router.post("/login", authMiddleware.validateLogin, authController.login);
+router.post("/auth/register", authMiddleware.validateRegister, authController.register);
+router.post("/auth/login", authMiddleware.validateLogin, authController.login);
 
 export default router;
